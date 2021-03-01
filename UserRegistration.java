5d9a2952e74f86177f4ce33ebceab303441a5264
unknown <kumar.skm03@gmail.com>
2021-03-01 16:20:16 +0530

    added method to determine valid last name in UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,9 +5,11 @@ public class UserRegistration {
 
 	private boolean check;
 	private String firstName;
+	private String lastName;
 
 	Scanner sc = new Scanner(System.in);
 
+	//UC1
 	public void checkFirstName() {
 		System.out.print("\nEnter First Name(starts with capital letter and write minimum 3 letter): ");
 		firstName = sc.nextLine();
@@ -20,8 +22,23 @@ public class UserRegistration {
 			System.out.print("First name is valid");
 	}
 
+
+        //UC2
+        public void checkLastName() {
+                System.out.print("\nEnter Last Name(starts with capital letter and write minimum 3 letter): ");
+                lastName = sc.nextLine();
+                boolean check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+                if (!check) {
+                        System.out.print("Invalid last name");
+                        checkLastName();
+                }
+                else
+                        System.out.print("Last name is valid");
+        }
+
 	public static void main(String args[]) {
 		UserRegistration user = new UserRegistration();
 		user.checkFirstName();
+		user.checkLastName();
 	}
 }
